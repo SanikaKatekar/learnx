719aa4c792541b42b13fd5e2237c677ea69706d7
SanikaKatekar <sanika.katekar@gmail.com>
2023-10-02 14:24:58 -0400

    added custom tag name to api route

--- src/api/routes/routes.py
+++ src/api/routes/routes.py
@@ -4,7 +4,7 @@ from services.services import GetStudyPlan
 
 router = APIRouter()
 
-@router.post("/generate_study_plan", response_model=ResponsePayload)
+@router.post("/generate-study-plan", response_model=ResponsePayload, tags=["Generate study plan with LearnX"])
 async def generate_study_plan(request_data: RequestPayload):
     try:
         # Call the service function with the request data
@@ -16,11 +16,10 @@ async def generate_study_plan(request_data: RequestPayload):
         raise HTTPException(status_code=500, detail=str(e))
 
 
+# router = APIRouter()
 
-router = APIRouter()
-
-@router.post("/study-plan")
-def study_plan(payload: RequestPayload):
-    study_plan_service = GetStudyPlan(payload)
-    result = study_plan_service.generate_plan()
-    return {"study_plan": result}
+# @router.post("/study-plan")
+# def study_plan(payload: RequestPayload):
+#     study_plan_service = GetStudyPlan(payload)
+#     result = study_plan_service.generate_plan()
+#     return {"study_plan": result}
